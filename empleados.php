272e6838e75470e1beb63d68ac3c1c9b4a44a14e
johaguzman9 <jandreinagrivas@hotmail.com>
2022-07-13 21:42:47 -0300

    cambios

--- empleados.php
+++ empleados.php
@@ -9,6 +9,7 @@ $aEmpleados[] = array ("dni"=> 33330675, "nombre"=>"David Garcia", "bruto"=> 850
 $aEmpleados[] = array ("dni"=> 40765908, "nombre"=>"Ana Del Valle", "bruto"=> 90000);
 $aEmpleados[] = array ("dni"=> 67324653, "nombre"=> "Andres Perez", "bruto"=>100000);
 $aEmpleados[] = array ("dni"=> 75334092, "nombre"=> "Victoria Luz", "bruto"=>70000);
+
 function calcularNeto ($bruto){
     return $bruto - ($bruto * 0.17);
 }
@@ -38,14 +39,15 @@ function calcularNeto ($bruto){
                     </tr>
                     <tbody>
                     <?php
-        foreach($aEmpleados as $empleado) { }?>  
+        foreach($aEmpleados as $empleado) { ?>  
                     <tr>
-                        <td><?php echo $empleado ["dni"]?></td>
-                        <td><?php echo mb_strtoupper ($empleado ["nombre"]) ?></td>
+                        <td><?php echo $empleado ["dni"];?></td>
+                        <td><?php echo mb_strtoupper ($empleado ["nombre"]); ?></td>
                         <td><?php echo number_format(calcularNeto ($empleado ["bruto"],2, ",", ".")); ?></td>
                     </tr>   
                     </tbody>
                 </thead>
+                <?php }?>
             </table>
     </div>
     </div>
